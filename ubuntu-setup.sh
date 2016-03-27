1582302e36eb74d0505cb13e3715fc6efd2976ea
Abdulrahman <d7e1001@gmail.com>
2016-03-27 08:35:28 +0300

    Update ubuntu-setup.sh

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -9,17 +9,5 @@ sudo /usr/bin/mysql_secure_installation
 
 sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
 
-#cat /etc/apache2/mods-enabled/dir.conf <<END
-#<IfModule mod_dir.c>
-#
-#          DirectoryIndex index.php index.html index.cgi index.pl index.php index.xhtml index.htm
-#
-#</IfModule>
-#END
 
-#cat /var/www/info.php <<END
-#<?php
-#phpinfo();
-#?>
-#END
 sudo service apache2 restart
