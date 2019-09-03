742371989863a42012a8ae0168e4de3cdcba2503
Abdulrahman <d7e1001@gmail.com>
2019-09-03 11:35:50 +0300

    Update ubuntu-setup.sh

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -7,7 +7,7 @@ sudo mysql_install_db
 
 sudo /usr/bin/mysql_secure_installation
 
-sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
+sudo apt install php libapache2-mod-php php-mysql
 
 
 sudo service apache2 restart
